7e3072ee827c307632f46c4bd9ac0aee66bc3597
AndAndrew <andrew.krivokhizhin@gmail.com>
2020-06-16 13:25:29 +0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Andrew K on 16.06.2020.
+//  Copyright © 2020 Andrew K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
